6d8fbd3c6b0d32ed5655d613087cdeaf770799ff
alexander <alexander.kapustin@quantumsoft.ru>
2020-01-21 19:43:36 +0300

    Impl distance for levenshtein

--- src/edit_based/levenshtein/wagner_fisher.rs
+++ src/edit_based/levenshtein/wagner_fisher.rs
@@ -1,5 +1,5 @@
+use crate::distance::Distance;
 use crate::error::SpatiumError;
-use crate::normalize::normalize;
 use std::cmp::Eq;
 
 use std::cmp::min;
@@ -8,19 +8,16 @@ type Result<T> = std::result::Result<T, SpatiumError>;
 
 /// [Wagner-Fisher](https://en.wikipedia.org/wiki/Wagner%E2%80%93Fischer_algorithm)
 /// implementation for Levenshtein distance.
-pub struct WagnerFisher {
-    /// Is result should be normalized?
-    normalized: bool,
-}
+pub struct WagnerFisher {}
 
 impl Default for WagnerFisher {
     fn default() -> Self {
-        Self { normalized: false }
+        Self {}
     }
 }
 
-impl WagnerFisher {
-    fn calc_distance<T>(&self, x: &[T], y: &[T]) -> Result<f64>
+impl Distance for WagnerFisher {
+    fn distance<T>(&self, x: &[T], y: &[T]) -> Result<f64>
     where
         T: Eq,
     {
@@ -59,35 +56,12 @@ impl WagnerFisher {
         }
         Ok(matrix[y_len][x_len] as f64)
     }
-
-    /// Set normalization parameter
-    ///
-    /// If normailzed is True,
-    /// the distance is normalized by dividing it
-    /// by the lenght of sequence.
-    pub fn normalize_result(mut self, normalized: bool) -> Self {
-        self.normalized = normalized;
-        self
-    }
-
-    /// Distance between sequences
-    pub fn distance<T>(&self, x: &[T], y: &[T]) -> Result<f64>
-    where
-        T: Eq,
-    {
-        let distance = self.calc_distance(x, y);
-
-        if self.normalized {
-            distance.and_then(|dis| normalize(dis, x.len(), y.len()))
-        } else {
-            distance
-        }
-    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::WagnerFisher;
+    use crate::distance::Distance;
 
     macro_rules! check_params {
 	($($name:ident: $value:expr,)*) => {
@@ -115,10 +89,7 @@ mod tests {
         let x = [1, 5, 3];
         let y = [4, 5, 6, 7];
 
-        let distance = WagnerFisher::default()
-            .normalize_result(true)
-            .distance(&x, &y)
-            .unwrap();
+        let distance = WagnerFisher::default().normalized_distance(&x, &y).unwrap();
         assert_eq!(distance, 3.0 / 4.0);
     }
 }
